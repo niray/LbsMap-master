5db03cf75fa9f618882abcce13b2eb1169efc6f1
zhining.chen <cznc@qq.com>
2017-02-27 16:47:20 +0800

    Initial commit

---
+++ app/src/main/java/com/shuyu/lbsmap/ClusterBaiduItem.java
@@ -0,0 +1,167 @@
+package com.shuyu.lbsmap;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.text.TextUtils;
+
+import com.baidu.mapapi.clusterutil.clustering.ClusterItem;
+import com.baidu.mapapi.clusterutil.clustering.view.DefaultClusterRenderer;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.model.LatLng;
+import com.shuyu.lbsmap.model.LBSModel;
+
+import java.io.File;
+
+import static com.shuyu.lbsmap.utils.FileUtils.BIG_END;
+
+/**
+ * 百度地图上一个一个的item
+ */
+public class ClusterBaiduItem implements ClusterItem {
+
+
+    //经纬度
+    private LatLng mPosition;
+
+    //对应的网络数据
+    private LBSModel mLBAModel;
+
+    //当前这个item的地址
+    private String markerAddress;
+
+    //聚合marker本地路径
+    private String urlClusterPath;
+
+    //当个marker本地路径
+    private String urlMarkerPath;
+
+    //marker的url
+    private String markerUrl;
+
+    //默认的marker图标
+    private int mBitmapId = -1;
+
+    public ClusterBaiduItem(LatLng latLng) {
+        mPosition = latLng;
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public BitmapDescriptor getBitmapDescriptor() {
+        if (mBitmapId != -1) {
+            return BitmapDescriptorFactory
+                    .fromResource(mBitmapId);
+        }
+
+        return BitmapDescriptorFactory
+                .fromResource(R.drawable.default_map_icon);
+    }
+
+    public String getUrlLocalMarkerIconPath() {
+        return urlMarkerPath;
+    }
+
+    public void setUrlMarkerPath(String urlMarkerPath) {
+        this.urlMarkerPath = urlMarkerPath;
+    }
+
+    public String getUrlClusterIconPath() {
+        return urlClusterPath;
+    }
+
+    public void setUrlClusterPath(String urlClusterPath) {
+        this.urlClusterPath = urlClusterPath;
+    }
+
+    public BitmapDescriptor getUrlClusterIconBitmapDescriptor() {
+        if (!TextUtils.isEmpty(urlClusterPath)) {
+            Bitmap var1 = BitmapFactory.decodeFile(urlClusterPath);
+            if (var1 == null) {
+                if (!DefaultClusterRenderer.LOADING_LOGO) deleteFile(urlClusterPath);
+                return null;
+            } else {
+                var1.recycle();
+            }
+            return BitmapDescriptorFactory
+                    .fromPath(urlClusterPath);
+
+        }
+
+        return null;
+    }
+
+    public BitmapDescriptor getUrlMarkerIconBitmapDescriptor(boolean select) {
+        if (select) {
+            if (!TextUtils.isEmpty(urlMarkerPath + BIG_END)) {
+                Bitmap var1 = BitmapFactory.decodeFile(urlMarkerPath + BIG_END);
+                if (var1 == null) {
+                    if (!DefaultClusterRenderer.LOADING_LOGO)
+                        deleteFile(urlMarkerPath + BIG_END);
+                    return null;
+                } else {
+                    var1.recycle();
+                }
+                return BitmapDescriptorFactory
+                        .fromPath(urlMarkerPath + BIG_END);
+            }
+        } else {
+            if (!TextUtils.isEmpty(urlMarkerPath)) {
+                Bitmap var1 = BitmapFactory.decodeFile(urlMarkerPath);
+                if (var1 == null) {
+                    if (!DefaultClusterRenderer.LOADING_LOGO)
+                        deleteFile(urlMarkerPath);
+                    return null;
+                } else {
+                    var1.recycle();
+                }
+                return BitmapDescriptorFactory
+                        .fromPath(urlMarkerPath);
+            }
+        }
+        return null;
+    }
+
+    private void deleteFile(String path) {
+        File file = new File(path);
+        if (file.exists()) {
+            try {
+                file.delete();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getMarkerAddress() {
+        return markerAddress;
+    }
+
+    public void setMarkerAddress(String markerAddress) {
+        this.markerAddress = markerAddress;
+    }
+
+    public void setBitmapId(int bitmapId) {
+        this.mBitmapId = bitmapId;
+    }
+
+    public String getMarkerUrl() {
+        return markerUrl;
+    }
+
+    public void setMarkerUrl(String markerUrl) {
+        this.markerUrl = markerUrl;
+    }
+
+    public LBSModel getLBAModel() {
+        return mLBAModel;
+    }
+
+    public void setLBAModel(LBSModel lbsModel) {
+        this.mLBAModel = lbsModel;
+    }
+}
---
+++ app/src/main/java/com/shuyu/lbsmap/MarkerActivity.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ */
+package com.shuyu.lbsmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.baidu.mapapi.clusterutil.clustering.ClusterManager;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMap.OnMapLoadedCallback;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MarkerActivity extends Activity implements OnMapLoadedCallback {
+
+    MapView mMapView;
+    BaiduMap mBaiduMap;
+    MapStatus ms;
+    private ClusterManager<ClusterBaiduItem> mClusterManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.baidu_map);
+        ms = new MapStatus.Builder().target(new LatLng(39.914935, 116.403119)).zoom(8).build();
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setOnMapLoadedCallback(this);
+        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(ms));
+        // 定义点聚合管理类ClusterManager
+        mClusterManager = new ClusterManager<ClusterBaiduItem>(this, mBaiduMap);
+        // 添加Marker点
+        addMarkers();
+        // 设置地图监听，当地图状态发生改变时，进行点聚合运算
+        mBaiduMap.setOnMapStatusChangeListener(mClusterManager);
+
+    }
+
+    @Override
+    protected void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    /**
+     * 向地图添加Marker点
+     */
+    public void addMarkers() {
+        // 添加Marker点
+        LatLng llA = new LatLng(39.963175, 116.400244);
+        LatLng llB = new LatLng(39.942821, 116.369199);
+        LatLng llC = new LatLng(39.939723, 116.425541);
+        LatLng llD = new LatLng(39.906965, 116.401394);
+        LatLng llE = new LatLng(39.956965, 116.331394);
+        LatLng llF = new LatLng(39.886965, 116.441394);
+        LatLng llG = new LatLng(39.996965, 116.411394);
+
+        List<ClusterBaiduItem> items = new ArrayList<>();
+        items.add(new ClusterBaiduItem(llA));
+        items.add(new ClusterBaiduItem(llB));
+        items.add(new ClusterBaiduItem(llC));
+        items.add(new ClusterBaiduItem(llD));
+        items.add(new ClusterBaiduItem(llE));
+        items.add(new ClusterBaiduItem(llF));
+        items.add(new ClusterBaiduItem(llG));
+
+        mClusterManager.addItems(items);
+
+    }
+
+    @Override
+    public void onMapLoaded() {
+        ms = new MapStatus.Builder().zoom(9).build();
+        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(ms));
+    }
+
+}
